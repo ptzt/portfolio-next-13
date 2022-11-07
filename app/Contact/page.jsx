33871becc0df8a4bb7c3776170df322315a073f7
Tomas <68767115+ptzt@users.noreply.github.com>
2022-11-07 08:33:15 -0300

    first example contact

--- app/Contact/page.jsx
+++ app/Contact/page.jsx
@@ -1,5 +1,54 @@
-export default function Contact() {
+'use client'
+import { useState } from 'react'
+import * as emailjs from 'emailjs-com'
+import styles from './Contact.module.css'
+
+const Talk = () => {
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [emailSent, setEmailSent] = useState(false);
+
+    const submit = () => {
+        if (email && message) {
+            const serviceId = process.env.serviceId;
+            const templateId = process.env.templatedId;
+            const userId = process.env.userId;
+            const templateParams = {
+                email,
+                message
+            };
+
+            emailjs.send(serviceId, templateId, templateParams, userId)
+                .then(response => console.log(response))
+                .then(error => console.log(error));
+
+            setEmail('');
+            setMessage('');
+            setEmailSent(true);
+        } else {
+            alert('Por favor rellene todos los campos');
+        }
+    }
     return (
-        <h1>Contact</h1>
-    )
-}
+        <>
+            <div >
+                <h1 className={styles.title}>Vamos a hablar!💬</h1>
+                <p className={styles.text}>Dejame un mensaje en el formulario a continuación o póngase en contacto a través de Discord o Twitter.</p>
+                <div className="main-box" id="contact-form">
+                    <label for email className="text">EMAIL</label>
+                    <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+                    <label for message className="text">TU MENSAJE</label>
+                    <textarea value={message} onChange={e => setMessage(e.target.value)}></textarea>
+                    <button onClick={submit}>Enviar</button>
+                    <div className='text-ok'><span className={emailSent ? 'visible' : null}>Gracias por tu mensaje, me pondre en contacto contigo en un abrir y cerrar de ojos!</span></div>
+                </div>
+                <div className="social">
+                    <ul>
+                    </ul>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Talk;
